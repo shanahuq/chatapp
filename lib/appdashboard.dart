f2bbd7e69595877d2870716757d309727eeb2d21
shanahuq <sshana5163@gmail.com>
2026-02-10 23:34:42 +0530

    10/2/26

--- lib/appdashboard.dart
+++ lib/appdashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_6/contactsofgiftsending.dart';
 
 class Appdashboard extends StatefulWidget {
   const Appdashboard({super.key});
@@ -18,7 +19,7 @@ class _AppdashboardState extends State<Appdashboard> {
             clipBehavior: Clip.none,
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 190),
+                padding: const EdgeInsets.only(left: 150),
                 child: Text(
                   'Mettiunlike',
                   style: TextStyle(
@@ -40,16 +41,16 @@ class _AppdashboardState extends State<Appdashboard> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 50, top: 90),
+                padding: const EdgeInsets.only(left: 30, top: 90),
                 child: Container(
                   height: 144,
-                  width: 390,
+                  width: 360,
                   child: Image.asset('assets/Rectangle 8.png'),
                 ),
               ),
               Positioned(
                 bottom: -40,
-                left: 100,
+                left: 95,
                 child: Container(
                   padding: const EdgeInsets.all(4),
                   decoration: BoxDecoration(
@@ -58,8 +59,8 @@ class _AppdashboardState extends State<Appdashboard> {
                     // shape: BoxShape.circle,
                   ),
                   child: Image.asset('assets/image 91.png', fit: BoxFit.fill),
-                  height: 120,
-                  width: 100,
+                  height: 110,
+                  width: 95,
                   // child: CircleAvatar(
                   //   radius: 40,
                   //   backgroundImage: AssetImage('assets/image 91.png'),
@@ -71,28 +72,211 @@ class _AppdashboardState extends State<Appdashboard> {
           Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 90, right: 250),
-                child: Text(
-                  'Number of \n followers',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
+                padding: const EdgeInsets.only(top: 90, left: 40),
+                child: Row(
+                  children: [
+                    Text(
+                      'Number of \n followers',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        '1k+',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Text(
+                        'Number of \n members',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        '10k+',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(right: 250,top: 10),
-                child: Text(
-                  'Number of \n likes',
+                padding: const EdgeInsets.only(left: 40, top: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      'Number of \n likes',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        '36k+',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Text(
+                        'Average \n users',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        '728/month',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              height: 138,
+              width: 313,
+              child: Image.asset('assets/image 11.png'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 230, top: 25),
+            child: Text(
+              'Milestones',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 90, top: 90),
+            child: Text(
+              'Payment options for members',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 70, top: 5),
+            child: Row(
+              children: [
+                Text(
+                  'Credit card/ Debit card',
                   style: TextStyle(
                     fontWeight: FontWeight.w400,
                     fontSize: 15,
                     color: Colors.white,
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 110),
+                  child: Container(
+                    height: 13,
+                    width: 26,
+                    child: Image.asset('assets/Component 1.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 70, top: 5),
+            child: Row(
+              children: [
+                Text(
+                  'Online Wallet',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 177),
+                  child: Container(
+                    height: 13,
+                    width: 26,
+                    child: Image.asset('assets/Component 1.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 70, top: 25),
+            child: Row(
+              children: [
+                Text(
+                  'Banking Details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 17,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 170),
+                  child: GestureDetector(
+                    onTap: () {
+                      Contactsofgiftsending;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Contactsofgiftsending(),
+                        ),
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Color(0xffB9B9B9),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/contactsofgiftsending.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Contactsofgiftsending extends StatefulWidget {
+  const Contactsofgiftsending({super.key});
+
+  @override
+  State<Contactsofgiftsending> createState() => _ContactsofgiftsendingState();
+}
+
+class _ContactsofgiftsendingState extends State<Contactsofgiftsending> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
